af473ed209bae7a64da601e94164018709b93641
Eriko-Kojima <eriko.kojima17@gmail.com>
2023-09-10 16:25:07 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
